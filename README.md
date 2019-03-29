7d4071ad2a529aac36d006bd8670c4c3c0227628
doc-hil <doc-hil@conis.de>
2019-03-29 15:01:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# tablevert-core
+WORK IN PROGRESS
+
+tablevert-service is GOING TO be a Spring-based RESTful service application for providing [tablevert] functionality.
+
+[tablevert]: https://github.com/tablevert 
+
+A more detailed description will follow.
---
+++ src/main/java/org/tablevert/service/TablevertWebServiceApp.java
@@ -0,0 +1,14 @@
+package org.tablevert.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablevertWebServiceApp {
+
+    public static void main(String[] args) {
+        System.out.println("Los geht's");
+        SpringApplication.run(TablevertWebServiceApp.class, args);
+    }
+
+}
